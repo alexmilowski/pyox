be7169723749b6f61176824e3e6361c6b10ab499
alex.milowski <alex.milowski@orange.com>
2017-07-06 10:54:56 -0700

    added log configuration and switched to use python logging for default output

--- examples/queuelog/queuelog.py
+++ examples/queuelog/queuelog.py
@@ -5,6 +5,9 @@ import sys
 import signal
 import json
 from time import sleep
+from datetime import datetime
+import logging
+import logging.config
 
 def parseAuth(value):
    if value is None or len(value)==0:
@@ -78,6 +81,27 @@ parser.add_argument(
    default='default_client',
    help="The client module to import")
 parser.add_argument(
+   '--log-config',
+   dest='log_config',
+   help="A logging configuration (json or ini file format)")
+parser.add_argument(
+   '--log-prefix',
+   dest='log_prefix',
+   default='queue',
+   help="The prefix to use for the queue log files")
+parser.add_argument(
+   '-q','--quiet',
+   dest='quiet',
+   action='store_true',
+   default=False,
+   help="Suppress the console log")
+parser.add_argument(
+   '--no-log-file',
+   dest='log_enabled',
+   action='store_false',
+   default=True,
+   help="Suppress the log file")
+parser.add_argument(
      '--interval',
      type=int,
      default=60*5,
@@ -106,11 +130,15 @@ if args.verbose:
 
 running = True
 
+logger = logging.getLogger(__name__)
+
 def default_client(parameters):
    def handler(info):
-      sys.stdout.write('\x1e')
-      sys.stdout.write(json.dumps(info))
-      sys.stdout.flush()
+      logger.info(json.dumps(info))
+      #sys.stdout.write('\x1e')
+      #sys.stdout.write(json.dumps(info))
+      #sys.stdout.write('\n')
+      #sys.stdout.flush()
    return handler
 
 parts = args.client.rsplit('.', 1)
@@ -119,6 +147,59 @@ if len(parts)==1:
 else:
    handler = getattr(__import__(parts[0]),parts[1])(args.parameters)
 
+if args.log_config is not None:
+   extpos = args.log_config.rfind('.')
+   if extpos>0 and args.log_config[extpos:]=='.json':
+      with open(args.log_config) as f:
+         config = json.load(f)
+      logging.config.dictConfig(config)
+   else:
+      logging.config.fileConfig(args.log_config)
+else:
+   config = {
+      'version' : 1,
+      'disable_existing_loggers' : False,
+      'formatters' : {
+         'console' : {
+            'format' : '%(message)s'
+         },
+         'jsonseq' : {
+            'format' : '\x1e%(message)s'
+         }
+      },
+      'handlers' : {
+         'stdout' : {
+            'level' : 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter' : 'console',
+            'stream' : 'ext://sys.stdout'
+
+         },
+         'file' : {
+            'level' : 'INFO',
+            'class' : 'logging.handlers.RotatingFileHandler',
+            'formatter' : 'jsonseq',
+            'filename' : args.log_prefix + '.log',
+            'maxBytes' : 100*1024*1024,
+            'encoding' : 'utf-8'
+         }
+      },
+      'loggers' : {
+         '' : {
+            'level' : 'INFO',
+            'handlers' : ['stdout','file'],
+            'propagate' : True
+         }
+      }
+   }
+   if args.quiet:
+      config['loggers']['']['handlers'].remove('stdout')
+   if not args.log_enabled:
+      config['loggers']['']['handlers'].remove('file')
+      config['handlers'].pop('file')
+
+   logging.config.dictConfig(config)
+
 def shutdown_handler(signal, frame):
    handler(None)
    sys.exit(0)
@@ -126,10 +207,23 @@ def shutdown_handler(signal, frame):
 signal.signal(signal.SIGINT, shutdown_handler)
 signal.signal(signal.SIGTERM, shutdown_handler)
 
+def leafQueue(info):
+   queues = info.get('queues')
+   if queues is not None:
+      for child in queues['queue']:
+         leafQueue(child)
+   if info['type']!="capacitySchedulerLeafQueueInfo":
+      return;
+   wrapper = {
+      'at' : datetime.now().isoformat(),
+      'queue' : info
+   }
+   handler(wrapper)
+
 while running:
    try:
       info = client.scheduler();
-      handler(info)
+      leafQueue(info)
    except ServiceError as err:
       sys.stderr.write(str(err))
    sleep(args.interval)
