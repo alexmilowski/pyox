0a5d604589ab18ca95616445aa7be7dce224e3e2
alex.milowski <alex.milowski@orange.com>
2017-08-11 12:05:54 -0700

    fixed missing app files

--- setup.py
+++ setup.py
@@ -76,7 +76,9 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'virtualenv']),
+    packages=['pyhadoopapi','pyhadoopapi.apps.monitor'],
+    #packages=['pyhadoopapi','pyhadoopapi.apps.monitor','pyhadoopapi.apps.monitor.templates','pyhadoopapi.apps.monitor.assets.css','pyhadoopapi.apps.monitor.assets.js'],
+    #find_packages(exclude=['contrib', 'docs', 'tests', 'virtualenv']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
