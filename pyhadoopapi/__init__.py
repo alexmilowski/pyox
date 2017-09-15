dcb69b24d24ccab87a55f737ccef013914905119
alex.milowski <alex.milowski@orange.com>
2017-09-15 16:09:46 -0700

    adjusted imports; bumped version

--- pyhadoopapi/__init__.py
+++ pyhadoopapi/__init__.py
@@ -1,10 +1,10 @@
-from .client import ServiceError
+from .client import Client,ServiceError,parse_args,make_client
 from .webhdfs import WebHDFS
 from .oozie import Oozie,Job,Workflow,InvalidWorkflow
 from .cluster import ClusterInformation
 __all__ = [
-   'ServiceError',
+   'Client','ServiceError','parse_args','make_client',
    'WebHDFS',
    'Oozie','Job','Workflow','InvalidWorkflow'
    'ClusterInformation']
-__version__ = '0.7.0'
+__version__ = '0.8.0'
